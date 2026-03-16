71d8f9fae0a0fca8c15dfeed128f97fd46ac46ed
Xurshidbek96 <muminovxurshidbek@gmail.com>
2026-03-16 12:40:38 +0500

    build: update dev-dist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.grrjb2f6u7k"
+    "revision": "0.dckhkgu2gs4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
