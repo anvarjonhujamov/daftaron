5f5f993a100f6aac0d9b57f3c735779abd269431
Xurshidbek96 <muminovxurshidbek@gmail.com>
2026-03-18 13:45:36 +0500

    feat: update application logo and favicon

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         react(),
         VitePWA({
             registerType: 'autoUpdate',
-            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            includeAssets: ['favicon.png', 'apple-touch-icon.png', 'mask-icon.svg'],
             manifest: {
                 name: 'Daftaron - Nasiya Boshqaruvi',
                 short_name: 'Daftaron',
