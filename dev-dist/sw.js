6d36bf1b414431a3f0d0bab85eba1f3ec9065d00
Xurshidbek96 <muminovxurshidbek@gmail.com>
2026-03-22 23:11:55 +0500

    debug: remove BlockedActionDrawer from CustomersPage.jsx

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4u44fq6gq9k"
+    "revision": "0.npf9j9j19q"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
