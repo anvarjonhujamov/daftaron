b9bc51dcb6320855f535d336ae70805637e55bf3
Xurshidbek96 <muminovxurshidbek@gmail.com>
2026-04-10 15:30:33 +0500

    chore: dev-dist yangilandi

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f3ddvdavo6"
+    "revision": "0.kql7tp2ga48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
