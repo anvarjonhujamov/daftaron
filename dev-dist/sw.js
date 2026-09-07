558a3700a2e0810d34da38051f29b682ba685f5b
Xurshidbek96 <muminovxurshidbek@gmail.com>
2026-09-07 10:33:44 +0500

    Edit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p9ssf4u5i5"
+    "revision": "0.idpd9nqa4go"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
